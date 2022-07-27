7e8f303bda9b6390ebe378e3e0353d6c021050d4
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-07-26 22:56:23 -0300

    Criando projeto para validação de documentos, criando projeto de teste e criando arquivo de solução

---
+++ DocumentValidation.Tests/DocumentValidationTests.cs
@@ -0,0 +1,144 @@
+namespace DocumentValidation.Tests;
+
+[TestClass]
+public class DocumentValidationTests
+{
+    // Validate Telephone
+    [TestMethod]
+    public void ValidateTelephone5Digits()
+    {
+        string telephone = "(35)35123-8146";
+
+        var validate = telephone.ValidateTelephone();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateTelephone4Digits()
+    {
+        string telephone = "(11)3456-8146";
+
+        var validate = telephone.ValidateTelephone();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateTelephone()
+    {
+        string telephone = "11)346481-46";
+
+        var validate = telephone.ValidateTelephone();
+        Assert.AreEqual(false, validate);
+    }
+
+    // Validate CEP
+
+    [TestMethod]
+    public void ValidateCepTrue()
+    {
+        string cep = "37540-000";
+        var validate = cep.ValidateCep();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateCepFalse()
+    {
+        string cep = "37540000";
+        var validate = cep.ValidateCep();
+        Assert.AreEqual(false, validate);
+    }
+
+    // Validate Email
+    [TestMethod]
+    public void ValidateEmailDot()
+    {
+        string email = "flavio.bergamini@hotmail.com";
+        var validate = email.ValidateEmail();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateEmailLine()
+    {
+        string email = "flavio_bergamini@hotmail.com";
+        var validate = email.ValidateEmail();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateEmailSingle()
+    {
+        string email = "flavio@hotmail.com.br";
+        var validate = email.ValidateEmail();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateEmailFalse()
+    {
+        string email = "@hotmail.com";
+        var validate = email.ValidateEmail();
+        Assert.AreEqual(false, validate);
+    }
+
+    [TestMethod]
+    public void ValidateEmailNoServer()
+    {
+        string email = "flavio";
+        var validate = email.ValidateEmail();
+        Assert.AreEqual(false, validate);
+    }
+
+    //Validate CNPJ
+
+    [TestMethod]
+    public void ValidateCnpjTrue()
+    {
+        string cnpj = "43.699.281/0001-25";
+        var validate = cnpj.ValidateCnpj();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateCnpjFalse()
+    {
+        string cnpj = "43699281/0001-25";
+        var validate = cnpj.ValidateCnpj();
+        Assert.AreEqual(false, validate);
+    }
+
+    [TestMethod]
+    public void ValidateCnpjLetter()
+    {
+        string cnpj = "43.6f9.2h1/0001-MB";
+        var validate = cnpj.ValidateCnpj();
+        Assert.AreEqual(false, validate);
+    }
+
+    //Validate CPF
+
+    [TestMethod]
+    public void ValidateCpfTrue()
+    {
+        string cpf = "000.999.888-77";
+        var validate = cpf.ValidateCpf();
+        Assert.AreEqual(true, validate);
+    }
+
+    [TestMethod]
+    public void ValidateCpfFalse()
+    {
+        string cpf = "00099988877";
+        var validate = cpf.ValidateCpf();
+        Assert.AreEqual(false, validate);
+    }
+
+    [TestMethod]
+    public void ValidateCpfLetter()
+    {
+        string cpf = "0F0.H99.M88-7B";
+        var validate = cpf.ValidateCpf();
+        Assert.AreEqual(false, validate);
+    }
+}
---
+++ DocumentValidation/Validate.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DocumentValidation;
+public static class Validate
+{
+    private static bool ValidateRule(string rule, string word){
+
+        var validate = new Regex(rule);
+
+        if (validate.IsMatch(word))
+            return true;
+        else
+            return false;
+    }
+    public static bool ValidateTelephone(this string telephone){
+        var regex = @"^\(([1-9]){2}\)(?:([0-9]{5})|([0-9]{4}))-([0-9]{4})$";
+        return ValidateRule(regex, telephone);
+    }
+
+    public static bool ValidateCep(this string cep){
+        var regex = @"^(([0-9]){5})-([0-9]{3})$";
+        return ValidateRule(regex, cep);
+    }
+
+    public static bool ValidateEmail(this string email){
+        var regex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        return ValidateRule(regex, email);
+    }
+
+    public static bool ValidateCnpj(this string cnpj){
+        var regex = @"^([0-9]{2}.[0-9]{3}.[0-9]{3}/[0-9]{4}-[0-9]{2})|([0-9]{14})$";
+        return ValidateRule(regex, cnpj);
+    }
+
+    public static bool ValidateCpf(this string cpf){
+        var regex = @"^([0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]{2})$";
+        return ValidateRule(regex, cpf);
+    }
+}
